dd0989d675d30ddfe0e25605a19e82c19a7e2f84
Ujjwal Priyadarshan <ujjwal.priyadarshan@live.com>
2025-09-29 21:51:09 +0000

    container based lambda with model generation for reuse

--- src_iac/main.tf
+++ src_iac/main.tf
@@ -25,26 +25,13 @@ locals {
             s3_bucket_name       = "ujp.landing.zone"
             lambda_function_name = "landing_handler"
             lambda_handler       = "landingzoneprocess.handler"
-            lambda_zip_file      = "lambda_landing.zip"
-            lambda_layers        = [
-                # aws_lambda_layer_version.lambda_layer.arn
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-pandas:25",
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-boto3:30",
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-numpy:16"
-            ]
+            lambda_img_source    = "895636586106.dkr.ecr.us-east-1.amazonaws.com/lambda_landing_img:latest"
         }
         ,{
             s3_bucket_name       = "ujp.curated.zone"
             lambda_function_name = "curated_handler"
             lambda_handler       = "curatedzoneprocess.handler"
-            lambda_zip_file      = "lambda_curated.zip"
-            lambda_layers        = [
-                # aws_lambda_layer_version.lambda_layer.arn
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-pandas:25",
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-boto3:30",
-                "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-numpy:16",
-                #"arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p310-scipy:3"
-            ]
+            lambda_img_source      = "895636586106.dkr.ecr.us-east-1.amazonaws.com/lambda_curated_img:latest"
         }
     ]
 }
@@ -55,9 +42,47 @@ module "s3_lambda" {
     source                = "./modules/s3_lambda"
     s3_bucket_name        = each.value.s3_bucket_name
     lambda_function_name  = each.value.lambda_function_name
-    lambda_zip_file       = each.value.lambda_zip_file
+    lambda_img_source       = each.value.lambda_img_source
     lambda_handler        = each.value.lambda_handler
     lambda_role_arn       = aws_iam_role.lambda_exec_role.arn
-    lambda_role_name      = aws_iam_role.lambda_exec_role.name
-    lambda_layers_arn     = each.value.lambda_layers
+    # lambda_role_name      = aws_iam_role.lambda_exec_role.name
+    # lambda_layers_arn     = each.value.lambda_layers
+}
+
+
+
+resource "aws_s3_bucket" "bucket_target" {
+  bucket = "ujp.target.zone"
+}
+
+resource "aws_iam_policy" "policy_s3" {
+  name        = "policy_target_s3"
+  description = "Allow access to bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+            "arn:aws:s3:::ujp.landing.zone",
+            "arn:aws:s3:::ujp.curated.zone",
+            "arn:aws:s3:::ujp.target.zone",
+            "arn:aws:s3:::ujp.landing.zone/*",
+            "arn:aws:s3:::ujp.curated.zone/*",
+            "arn:aws:s3:::ujp.target.zone/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_role_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.policy_s3.arn
 }
--- src_iac/modules/s3_lambda/main.tf
+++ src_iac/modules/s3_lambda/main.tf
@@ -7,11 +7,13 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_function_name
   role          = var.lambda_role_arn
-  handler       = var.lambda_handler
-  runtime       = var.lambda_runtime
-  filename      = var.lambda_zip_file
-  source_code_hash = filebase64sha256(var.lambda_zip_file)
-  layers        = var.lambda_layers_arn
+  # handler       = var.lambda_handler
+  # runtime       = var.lambda_runtime
+  # filename      = var.lambda_zip_file
+  package_type = "Image"
+  image_uri = var.lambda_img_source
+  # source_code_hash = filebase64sha256(var.lambda_zip_file)
+  # layers        = var.lambda_layers_arn
   timeout       = 60
 }
 
@@ -34,30 +36,30 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   depends_on = [aws_lambda_permission.allow_s3]
 }
 
-resource "aws_iam_policy" "read_s3" {
-  name        = "read_s3_${var.s3_bucket_name}"
-  description = "Allow Lambda to read from ${var.s3_bucket_name} bucket"
+# resource "aws_iam_policy" "read_s3" {
+#   name        = "read_s3_${var.s3_bucket_name}"
+#   description = "Allow Lambda to read from ${var.s3_bucket_name} bucket"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::${var.s3_bucket_name}",
-          "arn:aws:s3:::${var.s3_bucket_name}/*"
-        ]
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:ListBucket"
+#         ],
+#         Resource = [
+#           "arn:aws:s3:::${var.s3_bucket_name}",
+#           "arn:aws:s3:::${var.s3_bucket_name}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_s3_read_attach" {
-  role       = var.lambda_role_name
-  policy_arn = aws_iam_policy.read_s3.arn
-}
+# resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
+#   role       = var.lambda_role_name
+#   policy_arn = aws_iam_policy.read_s3.arn
+# }
